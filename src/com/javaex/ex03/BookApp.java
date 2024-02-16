817d1838a29c87c73908f9e6b9c90f8ca7b53d1c
hyeon0714 <xodid6960@naver.com>
2024-02-16 11:22:45 +0900

    .

---
+++ src/com/javaex/ex03/BookApp.java
@@ -0,0 +1,33 @@
+package com.javaex.ex03;
+
+public class BookApp {
+
+	public static void main(String[] args) {
+
+		//작가5명 등록
+		//작가 출력
+		
+		//작가2명 삭제
+		
+		//작가1명 수정
+		
+		//작가 출력
+		
+		//책 5권등록
+		
+		//책 수정
+		
+		//책 삭제
+		
+		//책 1권출력
+		
+		//책만 다 출력
+		
+        //책+작가 다출력		
+
+		
+		
+		
+	}
+
+}
